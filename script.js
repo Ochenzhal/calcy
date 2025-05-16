4c3432364a9652df3d2e52de2b6053c72d67736e
Ihor Ponomarov <bxhorizon@gmail.com>
2025-05-16 20:14:07 +0300

    Add calculator basic layout

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let numLeft
+let numRight
+let operator
+
 function add(a, b) {
   return a + b
 }
@@ -12,4 +16,11 @@ function multiply(a, b) {
 
 function divide(a, b) {
   return a / b
+}
+
+function operate() {
+  if(operator === '+') return add(numLeft, numRight);
+  if(operator === '-') return subtract(numLeft, numRight);
+  if(operator === '*') return multiply(numLeft, numRight);
+  if(operator === '/') return divide(numLeft, numRight); 
 }
